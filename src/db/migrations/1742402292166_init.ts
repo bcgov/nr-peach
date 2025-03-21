f2b6278a35e5064c7feb12a3b1546b64a1e501c6
Jeremy Ho <jujaga@gmail.com>
2025-03-20 17:55:54 -0700

    Add db util support for indexing, and triggers
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- src/db/migrations/1742402292166_init.ts
+++ src/db/migrations/1742402292166_init.ts
@@ -1,23 +1,94 @@
 import { Kysely, sql } from "kysely";
 
-import { withTimestamps } from "../utils";
+import {
+  createAuditLogTrigger,
+  createIndex,
+  createUpdatedAtTrigger,
+  dropAuditLogTrigger,
+  dropIndex,
+  dropUpdatedAtTrigger,
+  withTimestamps,
+} from "../utils";
 
 export async function up(db: Kysely<any>): Promise<void> {
-  // Create schema updated at trigger
-  await sql`CREATE or REPLACE function set_updated_at()
-    returns trigger
-    language plpgsql
-    as $$
-    begin
-      new."updated_at" = now();
-      return new;
-    end;
-    $$`.execute(db);
-
-  // Create PIES schema
+  // Create audit and pies schemas
+  await db.schema.createSchema("audit").ifNotExists().execute();
   await db.schema.createSchema("pies").ifNotExists().execute();
 
-  // Create PIES tables
+  // Create schema functions
+  await sql`CREATE OR REPLACE FUNCTION audit.if_modified_func() RETURNS trigger AS $body$
+    DECLARE
+      v_old_data json;
+      v_new_data json;
+
+    BEGIN
+      if (TG_OP = 'UPDATE') then
+        v_old_data := row_to_json(OLD);
+        v_new_data := row_to_json(NEW);
+        insert into audit.logged_actions ("schema_name", "table_name", "db_user", "updated_by_username", "action_timestamp", "action", "original_data", "new_data")
+        values (TG_TABLE_SCHEMA::TEXT, TG_TABLE_NAME::TEXT, SESSION_USER::TEXT, NEW."updated_by", now(), TG_OP::TEXT, v_old_data, v_new_data);
+        RETURN NEW;
+      elsif (TG_OP = 'DELETE') then
+        v_old_data := row_to_json(OLD);
+        insert into audit.logged_actions ("schema_name", "table_name", "db_user", "action_timestamp", "action", "original_data")
+        values (TG_TABLE_SCHEMA::TEXT, TG_TABLE_NAME::TEXT, SESSION_USER::TEXT, now(), TG_OP::TEXT, v_old_data);
+        RETURN OLD;
+      else
+        RAISE WARNING '[AUDIT.IF_MODIFIED_FUNC] - Other action occurred: %, at %', TG_OP, now();
+        RETURN NULL;
+      end if;
+
+    EXCEPTION
+      WHEN data_exception THEN
+        RAISE WARNING '[AUDIT.IF_MODIFIED_FUNC] - UDF ERROR [DATA EXCEPTION] - SQLSTATE: %, SQLERRM: %',SQLSTATE,SQLERRM;
+        RETURN NULL;
+      WHEN unique_violation THEN
+        RAISE WARNING '[AUDIT.IF_MODIFIED_FUNC] - UDF ERROR [UNIQUE] - SQLSTATE: %, SQLERRM: %',SQLSTATE,SQLERRM;
+        RETURN NULL;
+      WHEN others THEN
+        RAISE WARNING '[AUDIT.IF_MODIFIED_FUNC] - UDF ERROR [OTHER] - SQLSTATE: %, SQLERRM: %',SQLSTATE,SQLERRM;
+        RETURN NULL;
+
+    END;
+    $body$
+    LANGUAGE plpgsql
+    SECURITY DEFINER
+    SET search_path = pg_catalog, audit;`.execute(db);
+
+  await sql`CREATE OR REPLACE FUNCTION pies.set_updated_at_func()
+    RETURNS TRIGGER
+    LANGUAGE plpgsql
+    AS $$
+    BEGIN
+      new."updated_at" = now();
+      RETURN new;
+    END;
+    $$`.execute(db);
+
+  // Create audit tables
+  await db.schema
+    .withSchema("audit")
+    .createTable("logged_actions")
+    .addColumn("id", "integer", (col) =>
+      col.primaryKey().generatedAlwaysAsIdentity()
+    )
+    .addColumn("schema_name", "text", (col) => col.notNull())
+    .addColumn("table_name", "text", (col) => col.notNull())
+    .addColumn("db_user", "text", (col) => col.notNull())
+    .addColumn("updated_by_username", "text")
+    .addColumn("action_timestamp", "timestamp", (col) =>
+      col.notNull().defaultTo(sql`now()`)
+    )
+    .addColumn("action", "text", (col) => col.notNull())
+    .addColumn("original_data", "json")
+    .addColumn("new_data", "json")
+    .execute();
+  await createIndex(db, "audit", "logged_actions", ["schema_name"]);
+  await createIndex(db, "audit", "logged_actions", ["table_name"]);
+  await createIndex(db, "audit", "logged_actions", ["action_timestamp"]);
+  await createIndex(db, "audit", "logged_actions", ["action"]);
+
+  // Create PIES tables and triggers
   await db.schema
     .withSchema("pies")
     .createTable("concept")
@@ -27,19 +98,16 @@ export async function up(db: Kysely<any>): Promise<void> {
     .addColumn("class", sql`_text`, (col) => col.notNull())
     .addColumn("concept", "text", (col) => col.notNull())
     .addColumn("version", "text", (col) => col.notNull())
-    .addUniqueConstraint("concept_class_concept_version_unique", [
+    .addUniqueConstraint("concept_class_concept_version_uk", [
       "class",
       "concept",
       "version",
     ])
     .$call(withTimestamps)
     .execute();
-  await db.schema
-    .withSchema("pies")
-    .createIndex("concept_class_index")
-    .on("concept")
-    .columns(["class"])
-    .execute();
+  await createIndex(db, "pies", "concept", ["class"]);
+  await createUpdatedAtTrigger(db, "pies", "concept");
+  await createAuditLogTrigger(db, "pies", "concept");
 
   await db.schema
     .withSchema("pies")
@@ -55,7 +123,11 @@ export async function up(db: Kysely<any>): Promise<void> {
       col.notNull().defaultTo(false)
     )
     .addColumn("concept_id", "integer", (col) =>
-      col.notNull().references("concept.id")
+      col
+        .notNull()
+        .references("concept.id")
+        .onUpdate("cascade")
+        .onDelete("cascade")
     )
     .addColumn("status", "text")
     .addColumn("status_code", "text")
@@ -68,20 +140,37 @@ export async function up(db: Kysely<any>): Promise<void> {
     .on("process_event")
     .columns(["system_record_id"])
     .execute();
+  await createUpdatedAtTrigger(db, "pies", "process_event");
+  await createAuditLogTrigger(db, "pies", "process_event");
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
-  // Drop PIES tables
+  // Drop PIES tables and triggers
+  await dropAuditLogTrigger(db, "pies", "process_event");
+  await dropUpdatedAtTrigger(db, "pies", "process_event");
   await db.schema
     .withSchema("pies")
     .dropIndex("process_event_system_record_id_index")
     .execute();
   await db.schema.withSchema("pies").dropTable("process_event").execute();
 
-  await db.schema.withSchema("pies").dropIndex("concept_class_index").execute();
+  await dropAuditLogTrigger(db, "pies", "concept");
+  await dropUpdatedAtTrigger(db, "pies", "concept");
+  await dropIndex(db, "pies", "concept", ["class"]);
   await db.schema.withSchema("pies").dropTable("concept").execute();
-  // Drop PIES schema
+
+  // Drop audit tables
+  await dropIndex(db, "audit", "logged_actions", ["action"]);
+  await dropIndex(db, "audit", "logged_actions", ["action_timestamp"]);
+  await dropIndex(db, "audit", "logged_actions", ["table_name"]);
+  await dropIndex(db, "audit", "logged_actions", ["schema_name"]);
+  await db.schema.withSchema("audit").dropTable("logged_actions").execute();
+
+  // Drop schema functions
+  await sql`DROP FUNCTION IF EXISTS pies.set_updated_at_func`.execute(db);
+  await sql`DROP FUNCTION IF EXISTS audit.if_modified_func`.execute(db);
+
+  // Drop audit and pies schemas
   await db.schema.dropSchema("pies").execute();
-  // Drop schema updated at trigger
-  await sql`DROP FUNCTION IF EXISTS public.set_updated_at`.execute(db);
+  await db.schema.dropSchema("audit").execute();
 }
--- src/db/utils.ts
+++ src/db/utils.ts
@@ -1,11 +1,79 @@
 import { sql } from "kysely";
 
-import type { CreateTableBuilder } from "kysely";
+import type { CreateTableBuilder, Kysely } from "kysely";
+
+export function createAuditLogTrigger(
+  qb: Kysely<any>,
+  schema: string,
+  table: string
+) {
+  return sql`CREATE TRIGGER ${sql.raw(table)}_audit_au_trigger
+    AFTER UPDATE OR DELETE ON ${sql.id(schema, table)}
+    FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func();`.execute(qb);
+}
+
+export function createIndex(
+  qb: Kysely<any>,
+  schema: string,
+  table: string,
+  columns: string[]
+) {
+  return qb.schema
+    .withSchema(schema)
+    .createIndex(`${table}_${columns.join("_")}_index`)
+    .on(table)
+    .columns(columns)
+    .execute();
+}
+
+export function createUpdatedAtTrigger(
+  qb: Kysely<any>,
+  schema: string,
+  table: string
+) {
+  return sql`CREATE TRIGGER ${sql.raw(table)}_bu_trigger
+    BEFORE UPDATE ON ${sql.id(schema, table)}
+    FOR EACH ROW EXECUTE PROCEDURE pies.set_updated_at_func();`.execute(qb);
+}
+
+export function dropAuditLogTrigger(
+  qb: Kysely<any>,
+  schema: string,
+  table: string
+) {
+  return sql`DROP TRIGGER IF EXISTS ${sql.raw(table)}_bu_trigger ON ${sql.id(
+    schema,
+    table
+  )}`.execute(qb);
+}
+
+export function dropIndex(
+  qb: Kysely<any>,
+  schema: string,
+  table: string,
+  columns: string[]
+) {
+  return qb.schema
+    .withSchema(schema)
+    .dropIndex(`${table}_${columns.join("_")}_index`)
+    .execute();
+}
+
+export function dropUpdatedAtTrigger(
+  qb: Kysely<any>,
+  schema: string,
+  table: string
+) {
+  return sql`DROP TRIGGER IF EXISTS ${sql.raw(table)}_bu_trigger ON ${sql.id(
+    schema,
+    table
+  )}`.execute(qb);
+}
 
 /**
  * @function withTimestamps
  * @description Adds timestamps to a table builder.
- * @param qb The table builder to add timestamps to.
+ * @param {CreateIndexBuilder} qb The table builder to add timestamps to.
  * @returns {CreateTableBuilder} The table builder with timestamps added. Should be invoked within a $call.
  */
 export function withTimestamps<TB extends string>(
