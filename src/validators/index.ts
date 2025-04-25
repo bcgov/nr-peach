2f5d7939d89aad41bfe8f603026c23aec72b781f
Jeremy Ho <jujaga@gmail.com>
2025-04-24 17:11:14 -0700

    Add a schema cache to reduce network call volume
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- src/validators/index.ts
+++ src/validators/index.ts
@@ -7,17 +7,26 @@ import type { AnySchemaObject, Plugin } from 'ajv';
 import type { FormatsPluginOptions } from 'ajv-formats';
 
 const log = getLogger(import.meta.filename);
+const schemaCache: Record<string, AnySchemaObject> = {};
 
 /**
  * Loads a schema from the given URI and returns it as an AnySchemaObject.
- * @param uri The URI of the schema to load
+ * @param schema The URI of the schema to load
  * @returns The loaded schema as an AnySchemaObject
  */
-export async function loadSchema(uri: string): Promise<AnySchemaObject> {
-  log.verbose(uri);
-  const res = await fetch(uri);
-  if (!res.ok) throw new Error(`Failed to fetch schema from ${uri}`);
-  return (await res.json()) as AnySchemaObject;
+export async function loadSchema(schema: string): Promise<AnySchemaObject> {
+  const cached = schema in schemaCache;
+  log.verbose('loadSchema', { cached, schema });
+
+  if (!cached) {
+    const res = await fetch(schema);
+    if (!res.ok) throw new Error(`Failed to fetch schema from ${schema}`);
+
+    const schemaObject = (await res.json()) as AnySchemaObject;
+    schemaCache[schema] = schemaObject;
+  }
+
+  return schemaCache[schema];
 }
 
 /**
@@ -29,13 +38,14 @@ export async function loadSchema(uri: string): Promise<AnySchemaObject> {
 export async function validateSchema(
   schema: string | AnySchemaObject,
   data: unknown
-) {
+): Promise<{ valid: boolean; errors?: unknown }> {
   const ajv = new Ajv({ allErrors: true, loadSchema });
   // TS workaround: https://github.com/ajv-validator/ajv-formats/issues/85#issuecomment-2377962689
   const addFormats = formats as unknown as Plugin<FormatsPluginOptions>;
   addFormats(ajv);
 
   const def = typeof schema === 'string' ? await loadSchema(schema) : schema;
-  const validator = await ajv.compileAsync(def);
-  return validator(data);
+  const validate = await ajv.compileAsync(def);
+  const valid = validate(data) as boolean;
+  return { valid: valid, errors: validate.errors ?? undefined };
 }
