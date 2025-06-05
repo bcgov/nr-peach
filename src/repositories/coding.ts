361251279ca3b145dd55035036267ffc58a488c2
Jeremy Ho <jujaga@gmail.com>
2025-06-05 13:08:08 -0700

    Add preliminary coding repository support
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

---
+++ src/repositories/coding.ts
@@ -0,0 +1,19 @@
+import { BaseRepository } from './index.ts';
+
+import type { InsertObject, InsertQueryBuilder, Kysely, Selectable, Transaction } from 'kysely';
+import type { DB } from '../types/index.ts';
+
+export class CodingRepository extends BaseRepository<'pies.coding'> {
+  constructor(db?: Kysely<DB> | Transaction<DB>) {
+    super('pies.coding', db);
+  }
+
+  override upsert(
+    data: InsertObject<DB, 'pies.coding'>
+  ): InsertQueryBuilder<DB, 'pies.coding', Selectable<DB['pies.coding']>> {
+    return super
+      .upsert(data)
+      .onConflict((oc) => oc.constraint('coding_code_code_system_version_id_unique').doNothing())
+      .returningAll();
+  }
+}
