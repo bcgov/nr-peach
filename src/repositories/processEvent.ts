20d889b024c7662e2fe525d1dad7e811f135361b
Jeremy Ho <jujaga@gmail.com>
2025-06-06 17:16:08 -0700

    Implement preliminary pruning logic for Process Events
    This also lifts the code/codeSystem validation to happen earlier in the
    call in order to help with short circuiting and potential refactoring.
    
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- src/repositories/processEvent.ts
+++ src/repositories/processEvent.ts
@@ -1,10 +1,21 @@
+import { sql } from 'kysely';
+
 import { BaseRepository } from './index.ts';
 
-import type { Kysely, Transaction } from 'kysely';
+import type { DeleteQueryBuilder, DeleteResult, Kysely, Transaction } from 'kysely';
 import type { DB } from '../types/index.ts';
 
 export class ProcessEventRepository extends BaseRepository<'pies.processEvent'> {
   constructor(db?: Kysely<DB> | Transaction<DB>) {
     super('pies.processEvent', db);
+  }
+
+  /**
+   * Deletes all process event entities associated with a specific system record.
+   * @param systemRecordId - The ID of the system record whose process events should be deleted.
+   * @returns A query builder instance configured to delete the specified records.
+   */
+  prune(systemRecordId: number): DeleteQueryBuilder<DB, 'pies.processEvent', DeleteResult> {
+    return this.db.deleteFrom(this.tableName).where(sql.ref('system_record_id'), '=', systemRecordId);
   }
 }
