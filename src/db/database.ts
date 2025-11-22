c92ac5614790200eb5c0f1dfb9c9b316965303e6
Jeremy Ho <jujaga@gmail.com>
2025-11-21 17:09:00 -0800

    feat: Implement optional auto migration and seeding functionality
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- src/db/database.ts
+++ src/db/database.ts
@@ -1,7 +1,8 @@
 import { config } from 'dotenv';
+import { CamelCasePlugin, Kysely, Migrator, PostgresDialect, sql } from 'kysely';
+import { Seeder } from 'kysely-ctl';
 import { promises as dns } from 'node:dns';
 import { readdirSync } from 'node:fs';
-import { CamelCasePlugin, Kysely, Migrator, PostgresDialect, sql } from 'kysely';
 import { Pool, types } from 'pg';
 
 import { state } from '../state.ts';
@@ -56,10 +57,10 @@ export const db = new Kysely<DB>({
 });
 
 /** The Kysely migrator instance for managing database migrations. */
-export const migrator = new Migrator({
-  db: db,
-  provider: { getMigrations }
-});
+export const migrator = new Migrator({ db: db, provider: { getMigrations } });
+
+/** The Kysely seeder instance for managing seeding */
+export const seeder = new Seeder({ db: db, provider: { getSeeds } });
 
 /**
  * Checks the health status of the database by executing a simple query.
@@ -183,11 +184,49 @@ export function onLogEvent(event: LogEvent): void {
 /**
  * Handles errors emitted by the database connection pool.
  * Server assumes the worst and forces a database check on next database request.
- * @param err - The error object emitted by the pool.
+ * @param err The error object emitted by the pool.
  */
 export function onPoolError(err: Error): void {
   log.error(`Database has errored: ${err.message}`, { clientCount: pool.totalCount });
   state.ready = false;
+}
+
+/**
+ * Runs all missing database migrations
+ * @returns A promise that resolves to true if migrations succeeded, or false if they failed
+ */
+export async function runMigrations() {
+  const { error: migrateError, results: migrateResults } = await migrator.migrateToLatest();
+
+  migrateResults?.forEach((it) => {
+    if (it.status === 'Success') {
+      log.info(`Migration "${it.migrationName}" completed`, { migration: `${it.migrationName}.ts` });
+    } else if (it.status === 'Error') {
+      log.error(`Failed to execute migration "${it.migrationName}"`);
+    }
+  });
+
+  if (migrateError) log.error(migrateError);
+  return !migrateError;
+}
+
+/**
+ * Runs all missing seed operations
+ * @returns A promise that resolves to true if seeding succeeded, or false if they failed
+ */
+export async function runSeeds() {
+  const { error: seedError, results: seedResults } = await seeder.run();
+
+  seedResults?.forEach((it) => {
+    if (it.status === 'Success') {
+      log.info(`Seed "${it.seedName}" completed`, { seed: `${it.seedName}.ts` });
+    } else if (it.status === 'Error') {
+      log.error(`failed to execute seed "${it.seedName}"`);
+    }
+  });
+
+  if (seedError) log.error(seedError);
+  return !seedError;
 }
 
 /**
