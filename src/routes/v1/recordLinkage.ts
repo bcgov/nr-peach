e10bdf7f6534689dfcacaf819116f300ce2478ec
qhanson55 <quinn.hanson@gov.bc.ca>
2026-01-22 16:13:03 -0800

    feat(record linkage): implement preliminary record linkage support
    - Implemented add, delete, and list endpoints.
    
    Signed-off-by: qhanson55 <quinn.hanson@gov.bc.ca>

--- src/routes/v1/recordLinkage.ts
+++ src/routes/v1/recordLinkage.ts
@@ -1,35 +1,26 @@
 import { Router } from 'express';
 
+import {
+  deleteRecordLinkageController,
+  getRecordLinkagesController,
+  putRecordLinkageController
+} from '../../controllers/index.ts';
 import { authz } from '../../middlewares/index.ts';
-import { Problem } from '../../utils/index.ts';
 import {
   deleteRecordLinkagesSchemaValidator,
   getRecordLinkagesSchemaValidator,
   putRecordLinkagesSchemaValidator
 } from '../../validators/index.ts';
 
-import type { Request, Response } from 'express';
-
 const router = Router();
 
 /** Get Record Linkages */
-router.get('/record-linkages', getRecordLinkagesSchemaValidator, (req: Request, res: Response): void => {
-  new Problem(501).send(req, res);
-});
+router.get('/record-linkages', getRecordLinkagesSchemaValidator, getRecordLinkagesController);
 
 /** Put Record Linkages */
-router.put('/record-linkages', authz('body'), putRecordLinkagesSchemaValidator, (req: Request, res: Response): void => {
-  new Problem(501).send(req, res);
-});
+router.put('/record-linkages', authz('body'), putRecordLinkagesSchemaValidator, putRecordLinkageController);
 
 /** Delete Record Linkages */
-router.delete(
-  '/record-linkages',
-  deleteRecordLinkagesSchemaValidator,
-  authz('query'),
-  (req: Request, res: Response): void => {
-    new Problem(501).send(req, res);
-  }
-);
+router.delete('/record-linkages', authz('query'), deleteRecordLinkagesSchemaValidator, deleteRecordLinkageController);
 
 export default router;
