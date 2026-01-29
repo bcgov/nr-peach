ae079f11ada39560ad54389c08ab780779b6288f
Jeremy Ho <jujaga@gmail.com>
2026-01-28 17:27:38 -0800

    docs(openapi): Add RFC 6750 HTTP 400 and header responses to spec
    Includes HTTP 400, WWW-Authenticate headers and conditional appearance
    of the security and securitySchemes sections of the spec.
    
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- src/routes/docs.ts
+++ src/routes/docs.ts
@@ -3,44 +3,55 @@ import helmet from 'helmet';
 import { dump, load } from 'js-yaml';
 import { readFileSync } from 'node:fs';
 
+import { state } from '../state.ts';
 import { getDocHTML } from '../docs/index.ts';
 
 import type { Request, Response } from 'express';
+
+interface OpenAPISpec {
+  security?: [];
+  servers: { url: string }[];
+  components: {
+    securitySchemes?: {
+      OpenID: {
+        openIdConnectUrl?: string;
+      };
+    };
+  };
+}
 
 const router = Router();
 
 /** OpenAPI Docs */
 router.get(
   '/',
-  helmet({
-    contentSecurityPolicy: {
-      directives: {
-        'connect-src': [
-          "'self'", // eslint-disable-line quotes
-          'https://raw.githubusercontent.com'
-        ],
-        // @ts-expect-error ts(2322)
-        'img-src': [
-          "'self'", // eslint-disable-line quotes
-          'data:',
-          (_req: Request, res: Response): string => `'nonce-${res.locals.cspNonce}'`,
-          'https://cdn.redoc.ly'
-        ],
-        // @ts-expect-error ts(2322)
-        'media-src': [
-          "'self'", // eslint-disable-line quotes
-          'data:',
-          (_req: Request, res: Response): string => `'nonce-${res.locals.cspNonce}'`
-        ],
-        'script-src': [
-          'blob:',
-          "'unsafe-eval'" // eslint-disable-line quotes
-        ],
-        'script-src-elem': [
-          'https://cdn.redoc.ly',
-          "'unsafe-inline'" // eslint-disable-line quotes
-        ]
-      }
+  helmet.contentSecurityPolicy({
+    directives: {
+      'connect-src': [
+        "'self'", // eslint-disable-line quotes
+        'https://raw.githubusercontent.com'
+      ],
+      // @ts-expect-error ts(2322)
+      'img-src': [
+        "'self'", // eslint-disable-line quotes
+        'data:',
+        (_req: Request, res: Response): string => `'nonce-${res.locals.cspNonce}'`,
+        'https://cdn.redoc.ly'
+      ],
+      // @ts-expect-error ts(2322)
+      'media-src': [
+        "'self'", // eslint-disable-line quotes
+        'data:',
+        (_req: Request, res: Response): string => `'nonce-${res.locals.cspNonce}'`
+      ],
+      'script-src': [
+        'blob:',
+        "'unsafe-eval'" // eslint-disable-line quotes
+      ],
+      'script-src-elem': [
+        'https://cdn.redoc.ly',
+        "'unsafe-inline'" // eslint-disable-line quotes
+      ]
     }
   }),
   (_req: Request, res: Response): void => {
@@ -59,12 +70,22 @@ router.get('/openapi.json', (_req: Request, res: Response): void => {
 });
 
 /**
- * Gets the OpenAPI specification
+ * Gets the OpenAPI specification and updates security related fields based on the environment.
  * @returns The OpenAPI specification as an object
  */
-function getSpec(): unknown {
+function getSpec(): OpenAPISpec {
   const rawSpec = readFileSync('src/docs/openapi.yaml', 'utf8');
-  return load(rawSpec);
+  const spec = load(rawSpec) as OpenAPISpec;
+  spec.servers[0].url = '/api/v1';
+  if (process.env.AUTH_ISSUER) {
+    // eslint-disable-next-line max-len
+    spec.components.securitySchemes!.OpenID.openIdConnectUrl = `${process.env.AUTH_ISSUER}/.well-known/openid-configuration`;
+  }
+  if (state.authMode === 'none') {
+    delete spec.security;
+    delete spec.components.securitySchemes;
+  }
+  return spec;
 }
 
 export default router;
