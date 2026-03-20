7c29c36a87d0a07fdc789d44423466abaca310cb
Jeremy Ho <jujaga@gmail.com>
2026-03-20 00:03:16 -0700

    fix: add missing favicon.ico link in getDocHTML template
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- src/docs/docs.ts
+++ src/docs/docs.ts
@@ -30,6 +30,7 @@ export function getDocHTML(version = 'v1'): string {
       <!-- Needed for adaptive design -->
       <meta charset="utf-8"/>
       <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="icon" type="image/x-icon" href="/favicon.ico">
       <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700|Roboto:300,400,700" rel="stylesheet">
 
       <!-- ReDoc doesn't change outer page styles -->
